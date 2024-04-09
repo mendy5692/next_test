d69822ef68c96c53e5747bc1d8aa81a3203de7dc
mendy5692 <mmm0533115692@gmail.com>
2024-04-09 15:51:05 +0300

    creat loading skeleton

--- app/loading.js
+++ app/loading.js
@@ -1,3 +1,16 @@
+import { Skeleton } from "@mui/material";
+
 export default function Loading() {
-  return <div className="mmm">loading</div>;
+  return (
+    <div className="pageSkeleton">
+      <div className="searchSkeleton column">
+        <Skeleton animation="wave" width={"900px"} height={"200px"} sx={{ borderRadius: '28px' }}/>
+      </div>
+      <div className="gridSkeleton row">
+        <Skeleton animation="wave" width={"300px"} height={"600px"} sx={{ borderRadius: '16px' }}/>
+        <Skeleton animation="wave" width={"300px"} height={"600px"} sx={{ borderRadius: '16px' }}/>
+        <Skeleton animation="wave" width={"300px"} height={"600px"} sx={{ borderRadius: '16px' }}/>
+      </div>
+    </div>
+  );
 }
