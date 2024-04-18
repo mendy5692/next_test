c962997a026e0efa286f7488f1614e9b920da02a
mendy5692 <mmm0533115692@gmail.com>
2024-04-18 22:55:49 +0300

    fix

--- utils/aminRegister/adminRegister.js
+++ utils/aminRegister/adminRegister.js
@@ -1,38 +1,59 @@
-import "../../app/globals.css"
-import "./adminRegister.css"
-import { useState } from "react"
-
-
-
+"use client";
+import { useState } from "react";
+import "../../app/globals.css";
+import "./adminRegister.css";
 
 export default function AdminReg() {
-    const [page, setPage] = useState("art")
-    function updatePage(e){
-        setPage(e.target.value)
-        return 
-    }
-    async function handleSumbit(form_data) {
-    'use server'
-    }
+  const [page, setpage] = useState("");
   return (
     <div className="admin-reg-container column">
-        <select
-            className="select"
-            onChange={updatePage}
-            defaultValue={"art"}
-        >
-            <option value="art">name asc</option>
-            <option value="decorations">name des</option>
-            <option value="judaica">price asc</option>
-        </select>
-        <form className="admin-reg-form column" onSubmit={handleSumbit}>
-            <input name="title" type="text" placeholder="name..." min={2}></input>
-            <input name="manufacturer" type="text" placeholder="manu facturer..." min={2}></input>
-            <input name="desc" type="text" placeholder="description..." min={2}></input>
-            <input name="img_url" type="text" placeholder="uploud image..." min={2}></input>
-            <input name="price" type="number" placeholder="price..." min={1}></input>
-            <button type="submit">submit</button>
-        </form>
+      <select
+        className="select"
+        onChange={(e) => {
+          console.log(`coich: ${e.target.value}`);
+          setpage(e.target.value);
+        }}
+        defaultValue={"art"}
+      >
+        <option value="art">art</option>
+        <option value="decorations">decorations</option>
+        <option value="judaica">judaica</option>
+      </select>
+      <form
+        className="admin-reg-form column"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(`page: ${page}`);
+          
+        }}
+      >
+        <input name="title" type="text" placeholder="name..." min={2}></input>
+        <input
+          name="manufacturer"
+          type="text"
+          placeholder="manu facturer..."
+          min={2}
+        ></input>
+        <input
+          name="desc"
+          type="text"
+          placeholder="description..."
+          min={2}
+        ></input>
+        <input
+          name="img_url"
+          type="text"
+          placeholder="uploud image..."
+          min={2}
+        ></input>
+        <input
+          name="price"
+          type="number"
+          placeholder="price..."
+          min={1}
+        ></input>
+        <button type="submit">submit</button>
+      </form>
     </div>
-  )
+  );
 }
